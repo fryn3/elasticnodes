f8ea366f2139392332e4bbded196e0065408cfdc
Мгер <fryn3@mail.ru>
2018-05-26 14:27:57 +0300

    Создан классы для работы с автоматами

---
+++ automata.cpp
@@ -0,0 +1,6 @@
+#include "automata.h"
+
+Automata::Automata()
+{
+
+}
---
+++ automata.h
@@ -0,0 +1,11 @@
+#ifndef AUTOMATA_H
+#define AUTOMATA_H
+
+
+class Automata
+{
+public:
+    Automata();
+};
+
+#endif // AUTOMATA_H
