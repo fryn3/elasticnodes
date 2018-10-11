5cfbbfa957c0b022fa95cefe805a98fe78e55c9d
Мгер <fryn3@mail.ru>
2018-10-11 20:31:04 +0300

    inline функции для проверки ключей в json

--- jsonfuncs.h
+++ jsonfuncs.h
@@ -1,4 +1,24 @@
 #ifndef JSONFUNCS_H
 #define JSONFUNCS_H
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QDebug>
+
+inline bool missKey(const QJsonObject &json, QString key) {
+    if (!json.contains(key)) {
+        qWarning() << "QJsonObject не содержит " << key;
+        return true;
+    }
+    return false;
+}
+
+inline bool missKeys(const QJsonObject &json, QStringList keys) {
+    foreach (auto k, keys) {
+        if (missKey(json, k)) {
+            return true;
+        }
+    }
+    return false;
+}
 
 #endif // JSONFUNCS_H
